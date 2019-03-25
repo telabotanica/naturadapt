75a1bcf278ae64ec6f7133206b5e9b46e0591b1f
Hans Lemuet <Spone@users.noreply.github.com>
2019-03-25 18:12:33 +0100

    Load DoctrineFixturesBundle in all environments

--- config/bundles.php
+++ config/bundles.php
@@ -15,6 +15,6 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['all' => true],
     Knp\Bundle\GaufretteBundle\KnpGaufretteBundle::class => ['all' => true],
 ];
