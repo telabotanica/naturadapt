f84bb23830f4aa5fa1797793f6cc7d65b058bf6d
Maxime Cousinou <maxime@lmdlc.com>
2019-04-04 16:40:59 +0200

    use Postmark Transport for Swiftmailer

--- src/Service/EmailSender.php
+++ src/Service/EmailSender.php
@@ -7,14 +7,18 @@
 
 namespace App\Service;
 
+use Postmark\Transport;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+
 class EmailSender {
 	/**
 	 * @var \Swift_Mailer
 	 */
 	private $mailer;
 
-	public function __construct ( \Swift_Mailer $mailer ) {
-		$this->mailer = $mailer;
+	public function __construct ( \Swift_Mailer $mailer, ParameterBagInterface $params ) {
+		$transport    = new Transport( $params->get( 'postmark' )[ 'server_token' ] );
+		$this->mailer = new \Swift_Mailer( $transport );
 	}
 
 	public function send ( $from, $to, $subject, $message ) {
