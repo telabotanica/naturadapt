ca225fe3fded3c4772a209018cee79dd682ea592
Maxime Cousinou <maxime@lmdlc.com>
2019-06-13 19:12:05 +0200

    refactor templates to match design

---
+++ assets/js/ui/see-more.js
@@ -0,0 +1,10 @@
+import ready from 'mf-js/modules/dom/ready';
+
+ready( () => Array.from( document.querySelectorAll( '[data-see-more]' ) ).forEach( ( button ) => {
+	const element = button.nextElementSibling;
+
+	button.addEventListener( 'click', () => {
+		button.setAttribute( 'aria-hidden', 'true' );
+		element.setAttribute( 'aria-hidden', 'false' );
+	} );
+} ) );
