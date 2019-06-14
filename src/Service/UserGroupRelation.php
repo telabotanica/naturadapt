19abbe3a35f8e9c18e2591546bc9c8c66846e611
Maxime Cousinou <maxime.cousinou@zemax.net>
2019-06-14 16:51:30 +0200

    fix regression in git merge

--- src/Service/UserGroupRelation.php
+++ src/Service/UserGroupRelation.php
@@ -16,6 +16,21 @@ class UserGroupRelation {
 		$this->manager = $manager;
 	}
 
+	public function isAdmin ( ?User $user, Usergroup $group ) {
+		if ( empty( $user ) || !( $user instanceof User ) ) {
+			return FALSE;
+		}
+
+		if ( $user->isAdmin() ) {
+			return TRUE;
+		}
+
+		$membership = $this->manager->getRepository( UsergroupMembership::class )
+									->getMembership( $user, $group );
+
+		return !empty( $membership ) && ( $membership->getRole() === UsergroupMembership::ROLE_ADMIN );
+	}
+
 	public function isMember ( ?User $user, Usergroup $group ) {
 		return $this->manager->getRepository( UsergroupMembership::class )
 							 ->isMember( $user, $group );
