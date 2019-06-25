4178e438bbf31547961fbb393b01fde9d8dbbc2c
Maxime Cousinou <maxime.cousinou@zemax.net>
2019-06-25 16:50:31 +0200

    use buttons for menus

--- assets/js/ui/sibling-toggle.js
+++ assets/js/ui/sibling-toggle.js
@@ -3,7 +3,11 @@ import ready from 'mf-js/modules/dom/ready';
 ready( () => Array.from( document.querySelectorAll( '.sibling-toggle' ) ).forEach( ( button ) => {
 	const element = button.nextElementSibling;
 
+	const attribute = button.getAttribute( 'data-sibling-toggle' ) || 'aria-hidden';
+
 	button.addEventListener( 'click', () => {
-		element.setAttribute( 'aria-hidden', element.getAttribute( 'aria-hidden' ) === 'false' ? 'true' : 'false' );
+		const status = element.getAttribute( attribute ) === 'false' ? 'true' : 'false';
+		button.setAttribute( `data-sibling-${attribute}`, status );
+		element.setAttribute( attribute, status );
 	} );
 } ) );
