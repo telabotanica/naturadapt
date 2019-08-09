ba41ff325fd7eff029b9a28b79aa1684f198d24a
Maxime Cousinou <maxime.cousinou@zemax.net>
2019-08-09 14:20:16 +0200

    #87 Prefill Document name from file name

---
+++ assets/js/ui/input-file-prefill.js
@@ -0,0 +1,17 @@
+import ready from 'mf-js/modules/dom/ready';
+
+ready( () => Array.from( document.querySelectorAll( 'input[type="file"][data-prefill]' ) ).forEach( ( fileElement ) => {
+	const prefillElements = Array.from( document.querySelectorAll( fileElement.getAttribute( 'data-prefill' ) ) );
+
+	fileElement.addEventListener( 'change', ( e ) => {
+		const name = e.target.files[ 0 ].name
+			.replace( '_', ' ' )
+			.split( '.' )
+			.slice( 0, -1 )
+			.join( '.' );
+
+		prefillElements.forEach( ( input ) => {
+			input.value = name;
+		} );
+	} );
+} ) );
