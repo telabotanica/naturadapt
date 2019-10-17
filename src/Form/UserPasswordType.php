e14c201614940e8968e42a925d5f2c2224dac2d4
Maxime Cousinou <maxime.cousinou@zemax.net>
2019-10-17 14:02:35 +0200

    Email and Password changes (#143)
    * add Parameters route and Email Form
    
    * add Password Form
    
    * handle Password change
    
    * handle Email change

---
+++ src/Form/UserPasswordType.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Skill;
+use App\Entity\User;
+use App\Repository\SkillRepository;
+use App\Service\FileManager;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CountryType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
+
+class UserPasswordType extends AbstractType {
+	/**
+	 * UserEmailType constructor.
+	 */
+	public function __construct () {
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function buildForm ( FormBuilderInterface $builder, array $options ) {
+		$builder
+				->add( 'password', PasswordType::class )
+				->add( 'password_new', PasswordType::class, [ 'mapped' => FALSE ] )
+				->add( 'password_confirm', PasswordType::class, [ 'mapped' => FALSE ] )
+				->add( 'submit', SubmitType::class );
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function configureOptions ( OptionsResolver $resolver ) {
+		$resolver->setDefaults( [
+				'attr'       => [],
+				'data_class' => User::class,
+		] );
+	}
+}
