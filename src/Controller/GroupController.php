b0246d093f53ad1a1eaf7ba070c60db93346a071
Hans Lemuet <Spone@users.noreply.github.com>
2020-12-03 10:40:04 +0100

    Fix path to AccessDeniedException

--- src/Controller/GroupController.php
+++ src/Controller/GroupController.php
@@ -19,9 +19,9 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpKernel\Exception\AccessDeniedException;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class GroupController extends AbstractController {
 	/**************************************************
