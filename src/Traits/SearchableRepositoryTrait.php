d99930fa5198c96d08613151921ca94001dcb7e8
aschlegle <antoine.schlegel01@gmail.com>
2022-02-03 16:03:14 +0100

    Affichage groupes publics en hors connexion/privés et dont membre en connexion #336

--- src/Traits/SearchableRepositoryTrait.php
+++ src/Traits/SearchableRepositoryTrait.php
@@ -8,7 +8,14 @@ use Doctrine\ORM\QueryBuilder;
 trait SearchableRepositoryTrait
 {
 
-	protected function applyGroupsFilters(QueryBuilder &$qb, string $aliasLetter, array $groups, array $particularGroups)
+	/*
+	* @param QueryBuilder &$qb
+	* @param string $aliasLetter
+	* @param array $groups
+	* @param array $particularGroups
+	* @param string|null $currentUserId
+	*/
+	protected function applyGroupsFilters(QueryBuilder &$qb, string $aliasLetter, array $groups, array $particularGroups, $currentUserId)
 	{
 		/**
 		 * GROUPS AND PARTICULAR GROUPS
@@ -20,9 +27,37 @@ trait SearchableRepositoryTrait
 			$qb->setParameter('groupIds', $groupIds);
 		}
 		$qb->leftJoin('g.logo', 'l');
+
+		/**
+		 * PUBLIC OR PRIVATE GROUPS ACCORDING TO CONNEXION
+		 */
+		if (is_null($currentUserId)) {
+			$qb->andWhere($qb->expr()->eq('g.visibility', ':public'));
+		} else {
+			$qb->leftJoin('g.members', 'gM');
+
+			$qb->andWhere(
+				$qb->expr()->orX(
+					$qb->expr()->eq('g.visibility', ':public'),
+					$qb->expr()->eq('gM.user', ':currentUserId')
+				)
+			);
+			$qb->setParameter('currentUserId', $currentUserId);
+		}
+		$qb->setParameter('public', 'public');
+		$qb->groupBy('n.id');
 	}
 
-	public function searchFromIdsAndProperties(array $ids, array $groups, array $particularGroups, array $properties, $options = [])
+	/*
+	* @param array $ids
+	* @param array $groups
+	* @param array $particularGroups
+	* @param string|null $currentUserId
+	* @param array $properties
+	* @param array $options
+	* @return int
+	*/
+	public function searchFromIdsAndProperties(array $ids, array $groups, array $particularGroups, $currentUserId, array $properties, $options = [])
 	{
 		$options = array_merge(['page' => 0, 'limit' => 20], $options);
 		$qb = $this->createQueryBuilder('n');
@@ -36,22 +71,22 @@ trait SearchableRepositoryTrait
 		foreach ($properties as $property) {
 			switch ($property) {
 				case 'usergroup':
-					$this->applyGroupsFilters($qb, 'n', $groups, $particularGroups);
+					$this->applyGroupsFilters($qb, 'n', $groups, $particularGroups, $currentUserId);
 					array_push($editedProperties, 'g.name AS group_name', 'g.slug AS group_slug', 'l.path AS group_logo_path');
 					break;
 				case 'discussion':
 					$qb->innerJoin('n.discussion', 'd');
-					$this->applyGroupsFilters($qb, 'd', $groups, $particularGroups);
+					$this->applyGroupsFilters($qb, 'd', $groups, $particularGroups, $currentUserId);
 					array_push($editedProperties, 'd.title AS discussion_title', 'd.uuid AS discussion_uuid', 'g.name AS group_name', 'g.slug AS group_slug', 'l.path AS group_logo_path');
 					break;
 				case 'author':
 					$qb->innerJoin('n.author', 'a');
 					array_push($editedProperties, 'a.displayName AS author_name');
 					break;
-					// Name only appears for member category
 				case 'name':
+					// Name only appears for member category
 					$qb->innerJoin('n.usergroupMemberships', 'uM');
-					$this->applyGroupsFilters($qb, 'uM', $groups, $particularGroups);
+					$this->applyGroupsFilters($qb, 'uM', $groups, $particularGroups, $currentUserId);
 					array_push($editedProperties, 'n.' . $property);
 					// We add a group by to remove user which are in many particulars groups
 					$qb->groupBy('n.id');
@@ -69,7 +104,16 @@ trait SearchableRepositoryTrait
 			->getResult();
 	}
 
-	public function searchCountFromIdsAndProperties(array $ids, array $groups, array $particularGroups, array $properties, $options = [])
+	/*
+	* @param array $ids
+	* @param array $groups
+	* @param array $particularGroups
+	* @param string|null $currentUserId
+	* @param array $properties
+	* @param array $options
+	* @return int
+	*/
+	public function searchCountFromIdsAndProperties(array $ids, array $groups, array $particularGroups, $currentUserId, array $properties, $options = [])
 	{
 		$qb = $this->createQueryBuilder('n');
 		/**
@@ -81,11 +125,11 @@ trait SearchableRepositoryTrait
 		foreach ($properties as $property) {
 			switch ($property) {
 				case 'usergroup':
-					$this->applyGroupsFilters($qb, 'n', $groups, $particularGroups);
+					$this->applyGroupsFilters($qb, 'n', $groups, $particularGroups, $currentUserId);
 					break;
 				case 'discussion':
 					$qb->innerJoin('n.discussion', 'd');
-					$this->applyGroupsFilters($qb, 'd', $groups, $particularGroups);
+					$this->applyGroupsFilters($qb, 'd', $groups, $particularGroups, $currentUserId);
 					break;
 				case 'author':
 					$qb->innerJoin('n.author', 'a');
@@ -93,7 +137,7 @@ trait SearchableRepositoryTrait
 					// Name only appears for member category
 				case 'name':
 					$qb->innerJoin('n.usergroupMemberships', 'uM');
-					$this->applyGroupsFilters($qb, 'uM', $groups, $particularGroups);
+					$this->applyGroupsFilters($qb, 'uM', $groups, $particularGroups, $currentUserId);
 					// We add a group by to remove user which are in many particulars groups
 					$qb->addGroupBy('n.id');
 					break;
@@ -103,19 +147,12 @@ trait SearchableRepositoryTrait
 		}
 		// If name is in the properties, we are working with the member category.
 		// In that case the request is different: we want all the users which are at least one time in the filterGroupList, so we need distinct
-		if (in_array('name', $properties)) {
-			$r = count(
-				$qb->select('count(distinct n.id) as count')
-					->getQuery()
-					->getArrayResult()
-			);
-		} else {
-			$r = intval(
-				$qb->select('COUNT(n)')
-					->getQuery()
-					->getSingleScalarResult()
-			);
-		}
+		// if (in_array('name', $properties)) {
+		$r = count(
+			$qb->select('count(distinct n.id) as count')
+				->getQuery()
+				->getArrayResult()
+		);
 		return $r;
 	}
 }
