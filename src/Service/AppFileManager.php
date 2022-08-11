2bc00504a7b605efdfe92298582a4cab2a6059c3
aschlegle <antoine.schlegel01@gmail.com>
2022-08-11 18:08:01 +0200

    read.me changed + administrator file changed #341

--- src/Service/AppFileManager.php
+++ src/Service/AppFileManager.php
@@ -3,138 +3,142 @@
 namespace App\Service;
 
 use App\Entity\File;
-use App\Entity\Usergroup;
 use Doctrine\ORM\EntityManagerInterface;
 use Psr\Container\ContainerInterface;
+use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
-use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Yaml\Yaml;
 
-class AppFileManager {
-	private $manager;
+class AppFileManager
+{
+    private $manager;
 
-	/**
-	 * @var \Gaufrette\Filesystem $filesystem
-	 */
-	private $filesystem;
+    /**
+     * @var \Gaufrette\Filesystem
+     */
+    private $filesystem;
 
-	public function __construct ( EntityManagerInterface $manager, ContainerInterface $container, string $projectDir, string $assetPath ) {
-		$this->manager    = $manager;
-		$this->filesystem = $container->get( 'gaufrette.appfiles_filesystem' );
-		$this->projectDir = $projectDir;
-		$this->assetPath = $assetPath;
-	}
+    public function __construct(EntityManagerInterface $manager, ContainerInterface $container, string $projectDir, string $assetPath)
+    {
+        $this->manager = $manager;
+        $this->filesystem = $container->get('gaufrette.appfiles_filesystem');
+        $this->projectDir = $projectDir;
+        $this->assetPath = $assetPath;
+    }
 
-	public function getFileSystem () {
-		return $this->filesystem;
-	}
+    public function getFileSystem()
+    {
+        return $this->filesystem;
+    }
 
-	/**
-	 * Writes file in filesystem
-	 *
-	 * @param string                $file
-	 * @param                       $content
-	 *
-	 * @return bool|string
-	*/
-	public function writeFile ( string $file, string $directoryString, $content ) {
-		$filename  = pathinfo( $file, PATHINFO_FILENAME );
-		$extension = pathinfo( $file, PATHINFO_EXTENSION );
+    /**
+     * Writes file in filesystem.
+     *
+     * @param $content
+     *
+     * @return bool|string
+     */
+    public function writeFile(string $file, string $directoryString, $content)
+    {
+        $filename = pathinfo($file, PATHINFO_FILENAME);
+        $extension = pathinfo($file, PATHINFO_EXTENSION);
 
-		try {
-			$n = 1;
-			do {
-				$basename = $filename . ( $n > 1 ? '-' . $n : '' ) . '.' . $extension;
-				$fullname = $directoryString . '/' . $basename;
-				$n++;
-			} while ( $this->filesystem->getAdapter()->exists( $fullname ) );
+        try {
+            $n = 1;
+            do {
+                $basename = $filename.($n > 1 ? '-'.$n : '').'.'.$extension;
+                $fullname = $directoryString.'/'.$basename;
+                ++$n;
+            } while ($this->filesystem->getAdapter()->exists($fullname));
 
-			// $this->filesystem->delete('logo.png');
-			$this->filesystem->write( $fullname, $content );
+            // $this->filesystem->delete('logo.png');
+            $this->filesystem->write($fullname, $content);
 
-			return $fullname;
-		} catch ( FileException $e ) {
-			return FALSE;
-		}
-	}
+            return $fullname;
+        } catch (FileException $e) {
+            return false;
+        }
+    }
 
-	/**
-	 * Writes uploaded file in file system
-	 *
-	 * @param \Symfony\Component\HttpFoundation\File\UploadedFile $file
-	 * @param \App\Entity\Usergroup                               $group
-	 *
-	 * @return bool|string
-	 */
-	public function moveUploadedFile ( UploadedFile $file, string $directoryString ) {
-		return $this->writeFile( $file->getClientOriginalName(), $directoryString, file_get_contents( $file->getRealPath() ) );
-	}
+    /**
+     * Writes uploaded file in file system.
+     *
+     * @return bool|string
+     */
+    public function moveUploadedFile(UploadedFile $file, string $directoryString)
+    {
+        return $this->writeFile($file->getClientOriginalName(), $directoryString, file_get_contents($file->getRealPath()));
+    }
 
-	/**
-	 * Writes uploaded file in file system and return File object
-	 *
-	 * @param \Symfony\Component\HttpFoundation\File\UploadedFile $uploadedFile
-	 *
-	 * @return \App\Entity\File
-	 */
-	public function changeWithUploadedFile ( UploadedFile $uploadedFile, string $directoryString) {
-		$filename;
+    /**
+     * Writes uploaded file in file system and return File object.
+     *
+     * @return \App\Entity\File
+     */
+    public function changeWithUploadedFile(UploadedFile $uploadedFile, string $directoryString)
+    {
+        $filename;
 
-		switch ($directoryString) {
-			case 'logo':
-				$filename = $this->moveUploadedFile( $uploadedFile, 'logos' );
-				break;
-			case 'front':
-				$filename = $this->moveUploadedFile( $uploadedFile, 'fronts' );
-				break;
-			case 'frontgroup':
-				$filename = $this->moveUploadedFile( $uploadedFile, 'frontsgroup' );
-				break;
-			case 'frontgroup':
-				$filename = $this->moveUploadedFile( $uploadedFile, 'frontsgroup' );
-				break;
-			case 'home':
-				$filename = $this->moveUploadedFile( $uploadedFile, 'home' );
-				break;
-			default:
-				$filename = $this->moveUploadedFile( $uploadedFile, 'logos' );
-				break;
-		}
+        switch ($directoryString) {
+            case 'logo':
+                $filename = $this->moveUploadedFile($uploadedFile, 'logos');
+                break;
+            case 'front':
+                $filename = $this->moveUploadedFile($uploadedFile, 'fronts');
+                break;
+            case 'frontgroup':
+                $filename = $this->moveUploadedFile($uploadedFile, 'frontsgroup');
+                break;
+            case 'frontgroup':
+                $filename = $this->moveUploadedFile($uploadedFile, 'frontsgroup');
+                break;
+            case 'home':
+                $filename = $this->moveUploadedFile($uploadedFile, 'home');
+                break;
+            default:
+                $filename = $this->moveUploadedFile($uploadedFile, 'logos');
+                break;
+        }
 
-		$file = new File();
-		$file->setFilesystem( File::APP_FILES );
-		$file->setName( $uploadedFile->getClientOriginalName() );
-		$file->setPath( $filename );
-		$file->setType( $uploadedFile->getMimeType() );
-		$file->setSize( $uploadedFile->getSize() );
+        $file = new File();
+        $file->setFilesystem(File::APP_FILES);
+        $file->setName($uploadedFile->getClientOriginalName());
+        $file->setPath($filename);
+        $file->setType($uploadedFile->getMimeType());
+        $file->setSize($uploadedFile->getSize());
 
-		return $file;
-	}
+        return $file;
+    }
 
-	public function setAppImageId(string $tab, string $imageType, int $imageId){
-		$adminYaml = Yaml::parse(file_get_contents($this->projectDir .'/var/admin-text/administration.yaml'));
-		$adminYaml[$tab][$imageType]['fileId'] =  $imageId;
-		$adminYaml = Yaml::dump($adminYaml, 5);
-		file_put_contents($this->projectDir .'/var/admin-text/administration.yaml', $adminYaml);
-	}
+    public function setAppImageId(string $tab, string $imageType, int $imageId)
+    {
+        $adminYaml = Yaml::parse(file_get_contents($this->projectDir.'/config/platform/config.yaml'));
+        $adminYaml[$tab][$imageType]['fileId'] = $imageId;
+        $adminYaml = Yaml::dump($adminYaml, 5);
+        file_put_contents($this->projectDir.'/config/platform/config.yaml', $adminYaml);
+    }
 
-	public function getAppImageId(string $tab, string $imageType){
-		$adminYaml = Yaml::parse(file_get_contents($this->projectDir .'/var/admin-text/administration.yaml'));
-		return $adminYaml[$tab][$imageType]['fileId'];
-	}
+    public function getAppImageId(string $tab, string $imageType)
+    {
+        $adminYaml = Yaml::parse(file_get_contents($this->projectDir.'/config/platform/config.yaml'));
 
-	public function getFileById(int $fileId): File{
-		$fileManager = $this->manager->getRepository( File::class );
-		return $fileManager->getFileById( $fileId);
-	}
+        return $adminYaml[$tab][$imageType]['fileId'];
+    }
 
-	public function getDefaultFile(string $tab, string $image_type){
-		$fullPath = $this->projectDir .'/'. $this->assetPath;
+    public function getFileById(int $fileId): File
+    {
+        $fileManager = $this->manager->getRepository(File::class);
 
-		$adminYaml = Yaml::parse(file_get_contents($this->projectDir .'/config/administration-default.yaml'));
-		return $fullPath . $adminYaml[$tab][$image_type]['fileId'];
+        return $fileManager->getFileById($fileId);
+    }
 
-	}
+    public function getDefaultFile(string $tab, string $image_type)
+    {
+        $fullPath = $this->projectDir.'/'.$this->assetPath;
 
+        $adminYaml = Yaml::parse(file_get_contents($this->projectDir.'/config/platform/default.config.yaml'));
+
+        return $fullPath.$adminYaml[$tab][$image_type]['fileId'];
+    }
 }
--- src/Service/AppTextManager.php
+++ src/Service/AppTextManager.php
@@ -5,59 +5,62 @@ namespace App\Service;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Yaml\Yaml;
 
-class AppTextManager {
-	private $manager;
+class AppTextManager
+{
+    private $manager;
 
+    public function __construct(EntityManagerInterface $manager, string $projectDir)
+    {
+        $this->manager = $manager;
+        $this->projectDir = $projectDir;
+    }
 
+    public function changeText($tab, $key, $value)
+    {
+        $adminYaml = Yaml::parse(file_get_contents($this->projectDir.'/config/platform/config.yaml'));
+        $adminYaml[$tab][$key] = $value;
+        $adminYaml = Yaml::dump($adminYaml, 5);
+        file_put_contents($this->projectDir.'/config/platform/config.yaml', $adminYaml);
+    }
 
-	public function __construct ( EntityManagerInterface $manager, string $projectDir) {
-		$this->manager    = $manager;
-		$this->projectDir = $projectDir;
+    public function getTabText($tab)
+    {
+        $adminYamlTab = Yaml::parse(file_get_contents($this->projectDir.'/config/platform/config.yaml'))[$tab];
+        $defaultAdminYamlTab = Yaml::parse(file_get_contents($this->projectDir.'/config/platform/default.config.yaml'))[$tab];
+        foreach ($adminYamlTab as $key => $text) {
+            if (is_null($text)) {
+                $adminYamlTab[$key] = $defaultAdminYamlTab[$key];
+            }
+        }
 
-	}
+        return $adminYamlTab;
+    }
 
+    public function getTabSectionText($tab, $section)
+    {
+        $adminYamlTab = Yaml::parse(file_get_contents($this->projectDir.'/config/platform/config.yaml'))[$tab][$section];
+        $defaultAdminYamlTab = Yaml::parse(file_get_contents($this->projectDir.'/config/platform/default.config.yaml'))[$tab][$section];
+        foreach ($adminYamlTab as $key => $text) {
+            if (is_null($text)) {
+                $adminYamlTab[$key] = $defaultAdminYamlTab[$key];
+            }
+        }
 
-	public function changeText($tab, $key, $value){
-		$adminYaml = Yaml::parse(file_get_contents($this->projectDir .'/var/admin-text/administration.yaml'));
-		$adminYaml[$tab][$key] =  $value;
-		$adminYaml = Yaml::dump($adminYaml, 5);
-		file_put_contents($this->projectDir .'/var/admin-text/administration.yaml', $adminYaml);
-	}
+        return $adminYamlTab;
+    }
 
-	public function getTabText($tab){
-		$adminYamlTab = Yaml::parse(file_get_contents($this->projectDir .'/var/admin-text/administration.yaml'))[$tab];
-		$defaultAdminYamlTab = Yaml::parse(file_get_contents($this->projectDir .'/config/administration-default.yaml'))[$tab];
-		foreach($adminYamlTab as $key => $text){
-			if(is_null($text)){
-				$adminYamlTab[$key] = $defaultAdminYamlTab[$key];
-			}
-		}
-		return $adminYamlTab;
-	}
+    public function changeLiens($tab, $liens, $liensType)
+    {
+        $adminYaml = Yaml::parse(file_get_contents($this->projectDir.'/config/platform/config.yaml'));
 
-	public function getTabSectionText($tab, $section){
-		$adminYamlTab = Yaml::parse(file_get_contents($this->projectDir .'/var/admin-text/administration.yaml'))[$tab][$section];
-		$defaultAdminYamlTab = Yaml::parse(file_get_contents($this->projectDir .'/config/administration-default.yaml'))[$tab][$section];
-		foreach($adminYamlTab as $key => $text){
-			if(is_null($text)){
-				$adminYamlTab[$key] = $defaultAdminYamlTab[$key];
-			}
-		}
-		return $adminYamlTab;
-	}
-
-	public function changeLiens($tab, $liens, $liensType){
-		$adminYaml = Yaml::parse(file_get_contents($this->projectDir .'/var/admin-text/administration.yaml'));
-
-		$adminYaml[$tab][$liensType]['liens'] = [];
-		foreach ( $liens as $index => $lienObj ) {
-			if(!is_null($lienObj->getNom()) & !is_null($lienObj->getLien())){
-				$adminYaml[$tab][$liensType]['liens'][$index]['nom'] =  $lienObj->getNom();
-				$adminYaml[$tab][$liensType]['liens'][$index]['lien'] =  $lienObj->getLien();
-			}
-		}
-		$adminYaml = Yaml::dump($adminYaml, 5);
-		file_put_contents($this->projectDir .'/var/admin-text/administration.yaml', $adminYaml);
-	}
-
+        $adminYaml[$tab][$liensType]['liens'] = [];
+        foreach ($liens as $index => $lienObj) {
+            if (!is_null($lienObj->getNom()) & !is_null($lienObj->getLien())) {
+                $adminYaml[$tab][$liensType]['liens'][$index]['nom'] = $lienObj->getNom();
+                $adminYaml[$tab][$liensType]['liens'][$index]['lien'] = $lienObj->getLien();
+            }
+        }
+        $adminYaml = Yaml::dump($adminYaml, 5);
+        file_put_contents($this->projectDir.'/config/platform/config.yaml', $adminYaml);
+    }
 }
