48811c42e6bd67eb557a849bf2584a83ad660051
aschlegle <antoine.schlegel01@gmail.com>
2023-04-24 16:56:10 +0200

    change colors function for regions

--- assets/js/map/map-home-region.js
+++ assets/js/map/map-home-region.js
@@ -3,14 +3,36 @@ import domready from 'mf-js/modules/dom/ready';
 
 let geojsonLayer; // Déclarez la variable en dehors de la fonction
 
-function getColor(membersCount) {
-  return membersCount == 1 ? '#08306b' :
-         membersCount > 500  ? '#08519c' :
-         membersCount > 200  ? '#2171b5' :
-         membersCount > 100  ? '#4292c6' :
-         membersCount == 1   ? '#6baed6' :
-         membersCount == 0  ? '#9ecae1' :
-                               '#c6dbef';
+function getColor(value) {
+  let color;
+  let alpha;
+
+  if (value === 0) {
+    color = 'rgba(255, 255, 255, 0)'; // Completely transparent for 0
+  } else if (value === 1) {
+    alpha = 0.2;
+    color = `rgba(255, 245, 230, ${alpha})`;
+  } else if (value < 10) {
+    alpha = 0.4;
+    color = `rgba(255, 215, 180, ${alpha})`;
+  } else if (value < 50) {
+    alpha = 0.6;
+    color = `rgba(255, 185, 130, ${alpha})`;
+  } else if (value < 100) {
+    alpha = 0.8;
+    color = `rgba(255, 155, 80, ${alpha})`;
+  } else if (value < 250) {
+    alpha = 1;
+    color = `rgba(255, 125, 30, ${alpha})`;
+  } else if (value < 500) {
+    alpha = 1;
+    color = `rgba(255, 95, 0, ${alpha})`;
+  } else {
+    alpha = 1;
+    color = `rgba(230, 75, 0, ${alpha})`; // Darker orange for >= 500
+  }
+
+  return color;
 }
 
 async function loadRegionsLayer(map, zoomLevel, membersDataPromise) {
@@ -35,51 +57,63 @@ async function loadRegionsLayer(map, zoomLevel, membersDataPromise) {
 
   
 function style(feature, membersData, zoomLevel) {
-
-  console.log('membersData', membersData);
-
-  const levelCode = feature.properties["LEVL_CODE"];
   // Note: Plus le zoomLevel est élévé plus le niveau de détail est grand
-  
-  let count = 0;
-  
   // Le niveau de Zoom défini les régions plus ou moins petite à colorer sur la carte
   let levelCodeToKeep;
   if(zoomLevel > 5) {
     levelCodeToKeep = 3;
-  } else {
+  } else if (zoomLevel > 3) {
     levelCodeToKeep = 2;
+  } else {
+    levelCodeToKeep = 1;
   }
 
-  if (levelCodeToKeep == 2) {
-    const level2RegionCode = feature.properties.NUTS_ID.substring(0, 4);
-    if (membersData.level2.countByRegion.hasOwnProperty(level2RegionCode)) {
-      count = membersData.level2.countByRegion[level2RegionCode];
-    }
-  } else {
-      const level3RegionCode = feature.properties.NUTS_ID;
-      if (membersData.level3.countByRegion.hasOwnProperty(level3RegionCode)) {
-        count = membersData.level3.countByRegion[level3RegionCode];
+  let count = 0;
+  if (levelCodeToKeep == 3 || levelCodeToKeep == 2) {
+    const levelCode = feature.properties["LEVL_CODE"];
+    
+    if (levelCodeToKeep == 2) {
+      const level2RegionCode = feature.properties.NUTS_ID.substring(0, 4);
+      if (membersData.level2.hasOwnProperty(level2RegionCode)) {
+        count = membersData.level2[level2RegionCode];
       }
-  }
-  let fillColor = null
-  if(levelCode === levelCodeToKeep) {
-    fillColor = getColor(count);
+    } else {
+        const level3RegionCode = feature.properties.NUTS_ID;
+        if (membersData.level3.hasOwnProperty(level3RegionCode)) {
+          count = membersData.level3[level3RegionCode];
+        }
+    }
+    let fillColor = null
+    if(levelCode === levelCodeToKeep) {
+      fillColor = getColor(count);
+        return {
+          fillColor: fillColor,
+          weight: 1,
+          opacity: 1,
+          color: 'white',
+          fillOpacity: 0.7,
+        };
+    } else {
+  
       return {
-        fillColor: fillColor,
-        weight: 1,
+        weight: 2,
         opacity: 1,
         color: 'white',
-        fillOpacity: 0.7,
+        dashArray: '3',
+        fillOpacity: 0
       };
+    }
   } else {
- 
+    const countryCode = feature.properties.ISO_A2;
+    if (membersData.level1.hasOwnProperty(countryCode)) {
+      count = membersData.level1[countryCode];
+    }
     return {
-      weight: 2,
+      fillColor: getColor(count),
+      weight: 1,
       opacity: 1,
       color: 'white',
-      dashArray: '3',
-      fillOpacity: 0
+      fillOpacity: 0.7,
     };
   }
 
@@ -87,26 +121,24 @@ function style(feature, membersData, zoomLevel) {
       
 async function fetchMapData() {
   const response = await fetch('/map_data');
-  const membersDataLevel3 =  await response.json();
+  const membersData =  await response.json();
 
   // Calculer les données de niveau 2
-  const membersDataLevel2 = {};
-  for (const countryCode in membersDataLevel3) {
-    for (const regionId in membersDataLevel3[countryCode]) {
-      const level2RegionId = regionId.substring(0, 4);
-      if (!membersDataLevel2[countryCode]) {
-        membersDataLevel2[countryCode] = {};
-      }
-      if (!membersDataLevel2[countryCode][level2RegionId]) {
-        membersDataLevel2[countryCode][level2RegionId] = 0;
-      }
-      membersDataLevel2[countryCode][level2RegionId] += membersDataLevel3[countryCode][regionId];
+  const level2Data = {};
+
+  let level3Data = membersData["level3"];
+  for (const regionId in level3Data) {
+    const level2RegionId = regionId.substring(0, 4);
+    if (!level2Data[level2RegionId]) {
+      level2Data[level2RegionId] = 0;
     }
+    level2Data[level2RegionId] += level3Data[regionId];
   }
 
   return {
-    level2: membersDataLevel2,
-    level3: membersDataLevel3,
+    level1: membersData["level1"],
+    level2: level2Data,
+    level3: membersData["level3"],
   };
 }
 
@@ -115,7 +147,6 @@ domready(async () => {
     const mapElement = document.getElementById('mapHomeRegionId');
     if (mapElement) {
       const initialZoom = 5;
-      // initMap();
 
       const mapRegions = L.map('mapHomeRegionId', {
         minZoom: 2, // Niveau de zoom minimal
@@ -129,7 +160,6 @@ domready(async () => {
         maxZoom: 10, 
       }).addTo(mapRegions);
   
-      // const membersCountByRegion = getMembersCountByRegion();
       // Chargez les données de la carte
       const membersDataPromise = await fetchMapData();
 
