202ea45965c4ca079781e1789cf968981a64f280
aschlegle <antoine.schlegel01@gmail.com>
2023-04-25 16:35:21 +0200

    remove approach description when radio button clicked

--- assets/js/ui/adaptative-approach-form.js
+++ assets/js/ui/adaptative-approach-form.js
@@ -3,21 +3,26 @@ import domready from "mf-js/modules/dom/ready";
 domready(() => {
     const hasAdaptativeApproach = document.querySelector('#user_profile_hasAdaptativeApproach');
     const adaptativeApproachLinkRow = document.querySelector('[class="form-row form-row__field form-row__field.adaptative-approach-link-row"]');
+    const adaptativeApproachDescription = document.querySelector('[class="form-row form-row__field form-row__field.adaptative-approach-description"]');
     var radio;
 
-    function toggleAdaptativeApproachLink() {
+    function toggleAdaptativeApproachLinkAndDescription() {
         radio = document.querySelector('input[name="user_profile[hasAdaptativeApproach]"]:checked');
 
         if (radio.value === '1') {
             adaptativeApproachLinkRow.style.display = '';
+            adaptativeApproachDescription.style.display = '';
             adaptativeApproachLinkRow.querySelector('input').removeAttribute('disabled');
+            adaptativeApproachDescription.querySelector('input').removeAttribute('disabled');
         } else {
             adaptativeApproachLinkRow.style.display = 'none';
+            adaptativeApproachDescription.style.display = 'none';
             adaptativeApproachLinkRow.querySelector('input').setAttribute('disabled', 'disabled');
+            adaptativeApproachDescription.querySelector('input').setAttribute('disabled', 'disabled');
         }
     }
     if(hasAdaptativeApproach){
-        hasAdaptativeApproach.addEventListener('change', toggleAdaptativeApproachLink);
-        toggleAdaptativeApproachLink();
+        hasAdaptativeApproach.addEventListener('change', toggleAdaptativeApproachLinkAndDescription);
+        toggleAdaptativeApproachLinkAndDescription();
     }
 });
